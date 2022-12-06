1c08490c3fe4edb6232d3ed963a6bb42c1475a89
David Heim <david.naasheim@gmail.com>
2022-12-06 12:44:53 +0100

    csharp project made

--- day6/AOU2022day6-powershell.ps1
+++ day6/AOU2022day6-powershell.ps1
@@ -4,15 +4,13 @@ $task1Solved = $false
 $task2Solved = $false
 
 for ($i = 3; $i -lt $chararray.Count; $i++) {
-    #task 1
-    $charset = $chararray[($i - 3)..$i]
-    if (($charset | Group-Object).name.count -eq 4 -and $task1Solved -eq $false) {
+    # task 1
+    if (($chararray[($i - 3)..$i] | Group-Object).name.count -eq 4 -and $task1Solved -eq $false) {
         $i + 1 # this is the answer for task 1
         $task1Solved = $true
     }
     #task 2
-    $charset2 = $chararray[($i - 13)..$i]
-    if($i -gt 13 -and ($charset2 | Group-Object).name.count -eq 14 -and $task2Solved -eq $false){
+    if($i -gt 13 -and ($chararray[($i - 13)..$i]| Group-Object).name.count -eq 14 -and $task2Solved -eq $false){
         $i + 1 # this is answer for task 2
         $task2Solved = $true
     }
