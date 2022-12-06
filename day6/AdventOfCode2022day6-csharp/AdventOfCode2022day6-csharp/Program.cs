35a423f8d9ceb29c463114d480b675609ff8cfc0
David Heim <david.naasheim@gmail.com>
2022-12-06 13:47:31 +0100

    day6 sharp

--- day6/AdventOfCode2022day6-csharp/AdventOfCode2022day6-csharp/Program.cs
+++ day6/AdventOfCode2022day6-csharp/AdventOfCode2022day6-csharp/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string line = System.IO.File.ReadAllText(@"C:\\Users\\davhei15\\repos\\AdventOfCode2022\\day6\AOC2022day6-puzzleinput.txt");
+bool task1 = true;
+for (int i = 4; i < line.Length; i++)
+{
+        if((line[(i - 4)..i]).Distinct().Count() == 4 && task1 ) {
+        Console.WriteLine(i);
+        task1 = false;
+    }
+    if (i > 14 && (line[(i - 14)..i]).Distinct().Count() == 14)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+}
