b1edfedc868d959d2544522a90bd49c703165d65
David <david.naasheim@gmail.com>
2022-12-07 22:14:16 +0100

    completed day 7 in powershell

--- day7/aoc2022day7-powershell.ps1
+++ day7/aoc2022day7-powershell.ps1
@@ -1,12 +1,12 @@
-#$stuff = get-content "C:\Users\davhei15\repos\AdventOfCode2022\day7\AOC2022day6-puzzleinput.txt"
-#$stuff = get-clipboard
-$keep # contains get-clipboard stuff
-[int]$indexnum = 0
+$filestuff = get-content "C:\Users\david\REPOS\AdventOfCode2022\day7\AOC2022day7-puzzleinput.txt"
+$stuff = $filestuff
+$linenumber = 0
+$Global:currentdir = "root"
+$thingarray = New-Object -TypeName System.Collections.ArrayList
+$folderarray = New-Object -TypeName System.Collections.ArrayList
 
-$arraylist = New-Object -TypeName System.Collections.ArrayList
-$hashtable = @{}
-$hashtable2 = @{}
-function Invoke-PuzzleCommand {
+# function for the CD commands
+function Invoke-PuzzleCD {
     param (
         [string]$command
     )
@@ -14,52 +14,85 @@ function Invoke-PuzzleCommand {
         "$ cd /" {
             $Global:currentdir = "root"
         }
-        "$ cd .." { $Global:currentdir = -join $Global:currentdir[0..($Global:currentdir.Length - 3)] }
-        "$ ls" { <#do nothing#> }
+        "$ cd .." { 
+            $last = $Global:currentdir.LastIndexOf('/')
+            $Global:currentdir = $Global:currentdir.Substring(0, $last)
+        }
         default { 
-            $Global:dirname = ($command -split " ")[2]
+            $dirname = ($command -split " ")[2]
             $Global:currentdir = $Global:currentdir + "/" + $dirname 
+            [void]$Global:folderarray.add(
+                [PSCustomObject]@{
+                    dirname    = $dirname
+                    linenumber = $linenumber
+                })
         }
     }
 }
+# function for the LS commands
+function Invoke-PuzzleLS {
+    param (
+        [int]$linenumber
+    )
+    #should get everything from the LS line to the next $ command
+    for ($i = $linenumber + 1; $i -lt $stuff.Count; $i++) {
+        if ($stuff[$i] -notlike '$*') {
+            $stuff[$i]
+        }
+        else { break }
+    }
+}
+#create thingarray, which consists, of folder contents + folder path
 foreach ($item in $stuff) {
     switch -wildcard ($item) {
-        "$*" { Invoke-PuzzleCommand $item }
-        "dir*" {}
-        default {
-            [int]$value, [string]$name = $item.split(" ")
-            if ($null -eq $hashtable.$Global:currentdir) {
-                
-                $object = ([PSCustomObject]@{
-                        name       = $Global:currentdir
-                        dirname    = $Global:dirname
-                        value      = $value
-                        indexnum   = $indexnum
-                        totalvalue = 0
-                    })
-                    [void]$arraylist.add($object)
-                    $hashtable.add($Global:currentdir, $value)
-                    $hashtable2.add($Global:currentdir, $object)
-                $indexnum  ++
-            }
-            else { 
-                $hashtable.($Global:currentdir) += $value
-                $arraylist[$arraylist.where({ $Global:currentdir -eq $_.name }).indexnum].value += $value
-            }
+        "$ ls" {
+            [void]$thingarray.Add([PSCustomObject]@{
+                    dir     = $currentdir
+                    content = Invoke-PuzzleLS -linenumber $linenumber
+                }
+            ) 
         }
+        "$ c*" { Invoke-PuzzleCD -command $item }
+        Default {}
     }
+    $linenumber ++
 }
-($arraylist).foreach({
-        $item = $_
-        $dirsum = 0
-        $selection = $arraylist.where({ $_.name -like "$($item.name)*" })
-        $selection.foreach({ $dirsum += $_.value })
-        $arraylist[$item.indexnum].totalvalue = $dirsum
-    })
-[int]$sum = 0
-$arraylist.where({ $_.totalvalue -lt 100000 }).foreach({ $sum += $_.totalvalue })
-"# previous wrongs"
-1482845
-994905
-"----- possible answer---------"
+
+$NewThingarray = New-Object -TypeName System.Collections.ArrayList
+# find the filesizetotal for all folders, ignore all dirs, just focus on the files and create a list with path,filesizetotal,content
+foreach ($thing in $thingarray) {
+    $value = 0
+    $thing.content.foreach({
+            if ($_ -like 'dir*') {}
+            else { $value += [int]($_.split(' ')[0]) }
+        })
+    [void]$NewThingarray.add(
+        [PSCustomObject]@{
+            dir           = $thing.dir
+            content       = $thing.content
+            filesizetotal = $value
+        })
+}
+$LastThingarray = New-Object -TypeName System.Collections.ArrayList
+# loop trough $newthingarray and add the filesize totals of all paths  that inscude its own path.
+foreach ($thing in $NewThingarray) {
+    $value = 0
+    $NewThingarray.where({ $_.dir -like "$($thing.dir)*" }).filesizetotal.foreach({ $value += $_ })
+    [void]$LastThingarray.Add([PSCustomObject]@{
+            FolderTotal   = $value
+            foldername    = $thing.dir
+            content       = $thing.content
+            filesizetotal = [int]$thing.filesizetotal
+        })
+}
+$sum = 0
+$LastThingarray.where({$_.FolderTotal -lt 100000}).FolderTotal.foreach({$sum += $_})
+# answer 1
 $sum
+# task 2
+$totaldiskspace = 70000000
+$spacerequired = 30000000
+$spaceused = $LastThingarray[0].FolderTotal
+$freespace = $totaldiskspace-$spaceused
+$spacetofree =  $spacerequired-$freespace
+($LastThingarray.FolderTotal | sort).where({$_ -gt $spacetofree})[0]
