3373005037b83edb537c342358f59332e085cbe1
David <david.naasheim@gmail.com>
2022-12-07 16:54:56 +0100

    day1 2021

---
+++ 2021/day1/aoc2021day1-csharp/aoc2021day1-csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] lines = System.IO.File.ReadAllLines(@"C:\Users\david\REPOS\AdventOfCode2022\2021\day1\aoc2021day1-puzzleinput.txt");
+
+# Task 1
+int counter = 0;
+int previous = 9999999;
+foreach (string line in lines)
+{
+    if (Int32.Parse(line) > previous)
+    {
+        counter++;
+    }
+    previous = Int32.Parse(line);
+}
+Console.WriteLine(counter);
