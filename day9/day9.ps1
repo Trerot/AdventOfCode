cacfcb335b5670c693a49da23a6a2c509dae50a1
David <david.naasheim@gmail.com>
2022-12-09 21:03:06 +0100

    commit

--- day9/day9.ps1
+++ day9/day9.ps1
@@ -5,10 +5,8 @@ $Global:H = 'H', 0, 0
 $Global:T = 'T', 0, 0
 # $H[1] $T[1] is X
 # $H[2] $T[2] is Y
-
 [void]$Global:Arraylist.add( "$($H[0]),$($H[1]),$($H[2])")
 [void]$Global:Arraylist.add( "$($T[0]),$($T[1]),$($T[2])")
-
 function Move-H {
     param (
         $item
@@ -30,6 +28,7 @@ function Move-T {
     )
     $X = 0
     $Y = 0
+
     switch (([string]($H[1] - $T[1]) + ',' + [string]($H[2] - $T[2]))) {
         '-2,-1' { $X--; $Y-- }
         '-2,0' { $X-- }
@@ -53,15 +52,91 @@ function Move-T {
         '2,0' { $X++ }
         '2,1' { $X++; $Y++ }
     }
-
+    
     $Global:T[1] += $X
     $Global:T[2] += $Y
-
     [void]$Arraylist.add( "$($Global:T[0]),$($Global:T[1]),$($Global:T[2])")
 }
+foreach ($line in $stuff) {
+    Move-H -item $line
+}
+#answer prints here
+($arraylist | Group-Object | where-object { $_.name -like "t*" }).count
+
+
+# Task 2
+$stuff = get-content 'C:\Users\david\REPOS\AdventOfCode2022\day9\mediumsampleset.txt'
+$Global:SnakeArray = New-Object -TypeName System.Collections.ArrayList
+$Global:SnakeMovesArray = New-Object -TypeName System.Collections.ArrayList
+$Global:previous = 0
+#creating the snake
+(0..9).foreach({
+        [void]$Global:SnakeArray.add(@(0, 0))
+    }) 
+
+
+function Move-H {
+    param (
+        $item
+    )
+    [char]$Direction, [int]$Distance = $item.split(" ")
+    for ($i = 0; $i -lt $Distance; $i++) {
+        switch ($Direction) {
+            "R" { $Global:SnakeArray[0][0] ++ }
+            "L" { $Global:SnakeArray[0][0] -- }
+            "U" { $Global:SnakeArray[0][1] ++ }
+            "D" { $Global:SnakeArray[0][1] -- }
+        }
+        [void]$Global:SnakeMovesArray.add( "0,$($Global:SnakeArray[0][0]),$($Global:SnakeArray[0][1])")
+        # snake stuff
+        # now to move the knots.
+        $Global:previous = 0
+        for ($j = 1; $j -lt $Global:SnakeArray.Count; $j++) {
+            Move-T -snakenumber $j
+        }
+    }
+}
+function Move-T {
+    param (
+        [int]$snakenumber
+    )
+    $X = 0
+    $Y = 0
+    $XString = [string]($Global:SnakeArray[$Global:previous][0] - $Global:SnakeArray[$snakenumber][0])
+    $YString = [string]($Global:SnakeArray[$Global:previous][1] - $Global:SnakeArray[$snakenumber][1])
+
+    switch (($XString + ',' + $YString)) {
+        '-2,-1' { $X--; $Y-- }
+        '-2,0' { $X-- }
+        '-2,1' { $X--; $Y++ }
+        '-1,-2' { $X--; $Y-- }
+        '-1,-1' {  }
+        '-1,0' {  }
+        '-1,1' {  }
+        '-1,2' { $X--; $Y++ }
+        '0,-2' { $Y-- }
+        '0,-1' {  }
+        '0,0' {  }
+        '0,1' {  }
+        '0,2' { $Y++ }
+        '1,-2' { $X++; $Y-- }
+        '1,-1' {  }
+        '1,0' {  }
+        '1,1' {  }
+        '1,2' { $X++; $Y++ }
+        '2,-1' { $X++; $Y-- }
+        '2,0' { $X++ }
+        '2,1' { $X++; $Y++ }
+    }
+    
+    $Global:SnakeArray[$snakenumber][0] += $X
+    $Global:SnakeArray[$snakenumber][1] += $Y
+    
+    $Global:SnakeMovesArray.add( "$snakenumber,$($Global:SnakeArray[$snakenumber][0]),$($Global:SnakeArray[$snakenumber][1])")
+    $Global:previous = $snakenumber
+}
+
 
 foreach ($line in $stuff) {
     Move-H -item $line
 }
-
-($arraylist | Group-Object | where-object { $_.name -like "t*"}).count
