7783fa7dfdbda1536e3e1d478e26c9b126cb2bd9
David <david.naasheim@gmail.com>
2022-12-18 19:53:25 +0100

    Update day10.ps1

--- day10/day10.ps1
+++ day10/day10.ps1
@@ -3,42 +3,72 @@ $stuff = get-content .\day10\input.txt
 
 $Global:cycleCounter = 0
 $Global:XValue = 1
+$Global:SpriteRange = 0,1,2
 $Global:multiCycle = 20
 $Global:StrengthArray = New-Object -TypeName System.Collections.ArrayList
+$Global:CRTArray = New-Object -TypeName System.Collections.ArrayList
+$Global:CRTLine = $null
+
 function Invoke-XMultiplier {
     if ($Global:cycleCounter -eq $Global:multiCycle) {
         $strength = $Global:XValue * $Global:multiCycle
-        $Global:StrengthArray.add($strength)
+        [void]$Global:StrengthArray.add($strength)
         $Global:multiCycle += 40
     }
     
 }
-function Process-Stuffline {
-    param (
-        $line
-    )
-    process {
-        if ($line -eq "noop") {
-            $value = $null 
-            $Global:cycleCounter ++
-            Invoke-XMultiplier
-        }
-        else {
-            [int]$value = $line.split('addx ')[1] 
-            for ($i = 0; $i -lt 2; $i++) {
-                $Global:cycleCounter ++
-                Invoke-XMultiplier
-            }
+function Invoke-NewCRTLine {
 
+    process {
+        if ($Global:CRTLine.Length -eq 40) {
+            $Global:CRTArray.add($Global:CRTLine)
+            $Global:CRTLine = $null
         }
-        $Global:XValue += $value
     }
 }
-foreach ($line in $stuff) {
-    Process-Stuffline -line $line
+function Invoke-CRTPixel {
+    process {
+        $linenum = $Global:CRTLine.Length
+        if ($Global:SpriteRange -contains $linenum) { $Global:CRTLine += "#" }
+        elseif ($Global:SpriteRange -notcontains $linenum) {
+            $Global:CRTLine += "." 
+        }
+        else { "this should not happen." }
+    } 
 }
+
+
+# the loop.
+foreach ($line in $stuff) {
+    if ($line -eq "noop") {
+        $value = $null
+
+        $Global:cycleCounter ++
+        Invoke-XMultiplier
+        $Global:SpriteRange = ($Global:XValue - 1),$Global:XValue,($Global:XValue + 1)
+        Invoke-NewCRTLine
+        Invoke-CRTPixel
+    }
+    else {
+        [int]$value = $line.split('addx ')[1] 
+        for ($i = 0; $i -lt 2; $i++) {
+            $Global:cycleCounter ++
+            Invoke-XMultiplier
+            $Global:SpriteRange = ($Global:XValue - 1)..($Global:XValue + 1)
+            Invoke-NewCRTLine
+            Invoke-CRTPixel
+        }
+    }
+    $Global:XValue += $value
+        
+}
+
 $sum = 0
-$strengtharray.foreach({$sum += $_})
+$strengtharray.foreach({ $sum += $_ })
 $sum
 
-# task 2
+$CRTArray
+$CRTLine
+
+
+
