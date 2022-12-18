773157595f5d016a460b03382711460b49a61ccf
David <david.naasheim@gmail.com>
2022-12-18 19:58:17 +0100

    Update day10.ps1

--- day10/day10.ps1
+++ day10/day10.ps1
@@ -3,7 +3,7 @@ $stuff = get-content .\day10\input.txt
 
 $Global:cycleCounter = 0
 $Global:XValue = 1
-$Global:SpriteRange = 0,1,2
+$Global:SpriteRange = 0, 1, 2
 $Global:multiCycle = 20
 $Global:StrengthArray = New-Object -TypeName System.Collections.ArrayList
 $Global:CRTArray = New-Object -TypeName System.Collections.ArrayList
@@ -17,36 +17,29 @@ function Invoke-XMultiplier {
     }
     
 }
-function Invoke-NewCRTLine {
-
-    process {
-        if ($Global:CRTLine.Length -eq 40) {
-            $Global:CRTArray.add($Global:CRTLine)
-            $Global:CRTLine = $null
-        }
-    }
-}
 function Invoke-CRTPixel {
     process {
+        # sets the spirit position
+        $Global:SpriteRange = ($Global:XValue - 1), $Global:XValue, ($Global:XValue + 1)
+        # creates a new CRTline if it hits 40
+        if ($Global:CRTLine.Length -eq 40) {
+            [void]$Global:CRTArray.add($Global:CRTLine)
+            $Global:CRTLine = $null
+        }
+        # sets the pixel to either # or .
         $linenum = $Global:CRTLine.Length
         if ($Global:SpriteRange -contains $linenum) { $Global:CRTLine += "#" }
         elseif ($Global:SpriteRange -notcontains $linenum) {
             $Global:CRTLine += "." 
         }
-        else { "this should not happen." }
     } 
 }
-
-
 # the loop.
 foreach ($line in $stuff) {
     if ($line -eq "noop") {
         $value = $null
-
         $Global:cycleCounter ++
         Invoke-XMultiplier
-        $Global:SpriteRange = ($Global:XValue - 1),$Global:XValue,($Global:XValue + 1)
-        Invoke-NewCRTLine
         Invoke-CRTPixel
     }
     else {
@@ -54,21 +47,17 @@ foreach ($line in $stuff) {
         for ($i = 0; $i -lt 2; $i++) {
             $Global:cycleCounter ++
             Invoke-XMultiplier
-            $Global:SpriteRange = ($Global:XValue - 1)..($Global:XValue + 1)
-            Invoke-NewCRTLine
             Invoke-CRTPixel
         }
     }
-    $Global:XValue += $value
-        
+    $Global:XValue += $value 
 }
-
+# the results
 $sum = 0
 $strengtharray.foreach({ $sum += $_ })
 $sum
-
+[void]$Global:CRTArray.add($Global:CRTLine)
 $CRTArray
-$CRTLine
 
 
 
