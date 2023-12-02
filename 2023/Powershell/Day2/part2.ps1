21fa11e2272786627f1a25d918549e54d68e5401
David <david.naasheim@gmail.com>
2023-12-02 19:08:15 +0100

    Update part2.ps1

--- 2023/Powershell/Day2/part2.ps1
+++ 2023/Powershell/Day2/part2.ps1
@@ -1,14 +1,13 @@
 $Content = get-content .\2023\Powershell\Day2\puzzleinput.txt
 $sum = 0
 $Content.foreach({
-                $hands = ($_ -split ':')[1] -split ";"
                 $blue = 1
                 $red = 1
                 $green = 1
-                ((($hands -split ',').TrimStart() | select-string 'blue') -replace "[^0-9]").foreach({ if([int]$_ -gt $blue){$blue = [int]$_} })
-                ((($hands -split ',').TrimStart() | select-string 'red') -replace "[^0-9]" ).foreach({ if([int]$_ -gt $red){$red = [int]$_}  })
-                ((($hands -split ',').TrimStart() | select-string 'green') -replace "[^0-9]").foreach({ if([int]$_ -gt $green){$green = [int]$_} })
-                $pow = $blue * $red * $green
-                $sum += $pow
+                $hands = (($_ -split ':')[1] -split ";" -split ',').trimstart()
+                (($hands | select-string 'blue') -replace "[^0-9]").foreach({ if ([int]$_ -gt $blue) { $blue = [int]$_ } })
+                (($hands | select-string 'red') -replace "[^0-9]" ).foreach({ if ([int]$_ -gt $red) { $red = [int]$_ } })
+                (($hands | select-string 'green') -replace "[^0-9]").foreach({ if ([int]$_ -gt $green) { $green = [int]$_ } })
+                $sum += $blue * $red * $green
         })
 $sum
