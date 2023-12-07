3617d0542e2142e0f4aa233962d87e076258d438
David <david.naasheim@gmail.com>
2023-12-07 22:35:59 +0100

    Working part 1

--- 2023/Powershell/Day7/part1.ps1
+++ 2023/Powershell/Day7/part1.ps1
@@ -1,6 +1,5 @@
-$collection = get-content .\2023\Powershell\Day7\testinput.txt
+$collection = get-content .\2023\Powershell\Day7\puzzleinput.txt
 $games = new-object -TypeName System.Collections.ArrayList
-$hashgames = @{}
 $cardRanks = @{
   'A' = 14
   'K' = 13
@@ -23,10 +22,6 @@ foreach ($line in $collection) {
   $hand, [int]$bid = $line -split ' '
   $group = $hand.ToCharArray() | Group-Object | Sort-Object count -Descending
   switch ($group.count) {
-    1 {
-      #'5 of a kind' this one fails, becasse 5 count is just 1 not 5
-      $HandPower = 7
-    }
     2 {
       if ($group[0].count -eq 4) {
         #'four of a kind'
@@ -65,68 +60,24 @@ foreach ($line in $collection) {
   $strenghtarray = $hand.ToCharArray().foreach({ $cardranks.[string]$_ })
 
   [void]$games.Add([PSCustomObject]@{
-      Hand          = $hand
-      Bid           = $bid
-      HandPower     = $HandPower
-      Rank          = 0
-      multiply      = 0
-      HandNumber    = $counter
-      Strenghtarray = $strenghtarray
+      Hand      = $hand
+      Bid       = $bid
+      HandPower = $HandPower
+      Rank      = 0
+      multiply  = 0
+      1         = $strenghtarray[0]
+      2         = $strenghtarray[1]
+      3         = $strenghtarray[2]
+      4         = $strenghtarray[3]
+      5         = $strenghtarray[4]
     })
-  $hashgames.Add($counter , [PSCustomObject]@{
-      Hand          = $hand
-      Bid           = $bid
-      HandPower     = $HandPower
-      Rank          = 0
-      multiply      = 0
-      HandNumber    = $counter
-      Strenghtarray = $strenghtarray
-    })
-  $counter++
 }
-
-# $cardrank
-# hashtable to quickly get cardrank of aces, king etc. example $cardrank.a $cardrank.k
-
-# $games
-# arraylist with hand, bid, handpower(ranking of possible hands) and multiply(bid*rank)
-# its values rank is empty, which is needed to populate multiply
-
-# to fill in rank we start with getting $games.count and $i = 0
-# highest rank has the same rank as the number of games
-# find all handpower 7, if its alone give it $games.count - $i as rank. then do $i++
-
-# else compare each card until you find the winner.
-# this is what strengtharray is for, all cards have a numerical value.
-# so first compare all the ones, then just add the next number, and pick the next largest, and so on and so on until you have a winner
-$RankCounter = $games.count
-for ($i = 7; $i -gt 0; $i--) {
-  # foreach rank it does this
-  $SameRankGames = $games.where({ $_.handpower -eq $i })
-  #if its just one in the rank do this.
-  if ($SameRankGames.count -eq 1) {
-    ($hashgames.($SameRankGames.HandNumber)).rank = $RankCounter
-    $RankCounter--
-  }
-  # if its many in the rank do this
-  else {
-    for ($cardnum = 0; $cardnum -lt 5; $cardnum++) {
-      $SameRankGames.foreach({})
-      <# Action that will repeat until the condition is met #>
-    }
-  }
-
+# sort it and give it rank and multiply
+$RankCounter = 1
+$games = $games | Sort-Object -Property handpower, '1', '2', '3', '4', '5'
+for ($i = 0; $i -lt $games.Count; $i++) {
+  $games[$i].rank = $RankCounter
+  $games[$i].multiply = $RankCounter * $games[$i].bid
+  $RankCounter++
 }
-
-
-
-
-<# Every hand is exactly one type. From strongest to weakest, they are:
-7 Five of a kind, where all five cards have the same label: AAAAA
-6 Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-5 Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-4 Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-3 Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-2 One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-1 High card, where all cards' labels are distinct: 23456
- #>
+($games.multiply | Measure-Object -Sum).sum
