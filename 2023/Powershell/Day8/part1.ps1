20ad83db0631bf2ec53be1aa45ecae0cbd3f9cf9
David Heim <david.naasheim@gmail.com>
2023-12-08 09:08:51 +0100

    formating part1

--- 2023/Powershell/Day8/part1.ps1
+++ 2023/Powershell/Day8/part1.ps1
@@ -1,12 +1,12 @@
 $stuff = get-content .\2023\Powershell\Day8\puzzleinput.txt
 
 $lrmoves = $stuff[0].ToCharArray()
-$ElementHash=@{}
-foreach($element in $stuff[2..($stuff.count)]){
-  $name,$leftright=$element -replace ' ' -split '='
-  $left,$right = $leftright -replace '[()]' -split ','
+$ElementHash = @{}
+foreach ($element in $stuff[2..($stuff.count)]) {
+  $name, $leftright = $element -replace ' ' -split '='
+  $left, $right = $leftright -replace '[()]' -split ','
   $ElementHash.Add(
-    $name,[PSCustomObject]@{
+    $name, [PSCustomObject]@{
       L = $left
       R = $right
     }
@@ -15,14 +15,14 @@ foreach($element in $stuff[2..($stuff.count)]){
 
 $start = 'AAA'
 $goal = 'ZZZ'
- $movecounter = 0
+$movecounter = 0
 $move = $start
 
 while ($move -ne $goal) {
   foreach ($lr in $lrmoves) {
     $move = $ElementHash.$move.$lr
     $movecounter++
-    if($move -eq $goal){
+    if ($move -eq $goal) {
       break
     }
   }
