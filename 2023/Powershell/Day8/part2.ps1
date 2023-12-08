37486cc6880c11ebbe6ef50b4474297611231cbf
David <david.naasheim@gmail.com>
2023-12-08 22:07:49 +0100

    try with lcd now

--- 2023/Powershell/Day8/part2.ps1
+++ 2023/Powershell/Day8/part2.ps1
@@ -1,5 +1,6 @@
 $stuff = get-content .\2023\Powershell\Day8\puzzleinput.txt
-
+$aNodes = New-Object -TypeName System.Collections.ArrayList
+$zNodes = New-Object -TypeName System.Collections.ArrayList
 $lrmoves = $stuff[0].ToCharArray()
 $ElementHash = @{}
 foreach ($element in $stuff[2..($stuff.count)]) {
@@ -11,10 +12,39 @@ foreach ($element in $stuff[2..($stuff.count)]) {
             R = $right
         }
     )
+    if ($name -like '*A') {
+        [void]$aNodes.add($name)
+    }
+    if ($name -like '*Z') {
+        [void]$zNodes.add($name)
+    }
+
+}
+#now find count between Zs and the name of those Zs
+$moveArray = New-Object -TypeName System.Collections.ArrayList
+
+foreach ($a in $aNodes) {
+    $movecounter = 0
+    $move = $a
+    while ($move -notin $zNodes) {
+        foreach ($lr in $lrmoves) {
+            $move = $ElementHash.$move.$lr
+            $movecounter++
+            if ($move -in $zNodes) {
+                $moveArray.Add(
+                    [PSCustomObject]@{
+                        Name   = $move
+                        count  = $movecounter
+                        origin = $a
+                    }
+                )
+                break
+            }
+        } 
+    }
 }
 
-# i tried bruteforcing for 30 mins, this is in years or somethnig like that.
-# number is supposedly very large. so bruteforce not available, need some clever way.
+# i tried bruteforcing for 30 mins, this is in years or somethnig like that. need some clever way.
 
 # im thinking. for each path, find amount of moves between each '*z' and if it loops perfectly someplace,
 
